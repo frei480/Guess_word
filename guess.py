4e594b43cd7dad48a0b3e073764cd587d8f3cc7b
frei480 <frei741@gmail.com>
2024-06-16 20:43:59 +0500

    Update guess.py
    score system

--- guess.py
+++ guess.py
@@ -3,55 +3,63 @@ from random import randrange, shuffle
 from os import system, name
 
 
-clear:str = ''
-score:int = 0
+clear: str = 'cls' if name == 'nt' else 'clear'
+score: int = 0
+
+
 def guess(gstr: str):
+    global score
     field = ['_']*len(gstr)
     sh_char = list(set(gstr))
     shuffle(sh_char)
     sh_w: str = ', '.join(sh_char[:int(len(gstr)/2)])
     wrong: set[str] = set()
-    tries_max = len(gstr)+2
-    tries = 1
-    while True:
-        str_field = ''.join(field)
-        system(clear_os())
+
+    def print_table():
+        system(clear)
+        print(f'Очки: {score}')
         print(f'Подсказка: используемые буквы: {sh_w} ')
         if wrong:
             print(f'Не подходят: {", ".join(wrong)}')
         print(f'Слово: {str_field}')
+    while True:
+        str_field = ''.join(field)
+        print_table()
         char = input('Введи букву или слово целиком> ')
         if char == gstr:
             print(f'Отгадал! Это действительно было: {gstr}')
+            score += 5
             break
         for i, ch in enumerate(gstr):
             if ch == char:
                 field[i] = ch
+                if char not in sh_w:
+                    score += 1
         if char not in gstr:
             wrong.add(char)
-        if '_' in field:
-            tries += 1
-            if tries == tries_max:
-                print(f'Не отгадал! Загадано было: {gstr}')
-                return
-            continue
-        else:
+            score -= 1
+        if '_' not in field:
+            print_table()
             print(f'Отгадал! Слово было: {gstr}')
             break
-
-def clear_os():
-    return 'cls' if name == 'nt' else 'clear'
+        if score == 0:
+            print_table()
+            print(f'Очки закончились! Начинаем заново!')
+            score = 10
+            return
 
 
 def main():
-    with open('words.txt', mode='r' , encoding='utf-8') as f:
+    global score
+    with open('words.txt', mode='r', encoding='utf-8') as f:
         str_words = f.read()
         words = [w.strip() for w in str_words.split(',')]
-    system(clear_os())
+    system(clear)
+    score += 10
     while True:
         enter = input('Сыграем? Если да, нажми Enter')
         if enter == 'д' or enter == '':
-            system(clear_os())
+            system(clear)
             guess(words[randrange(0, len(words))])
         else:
             return
